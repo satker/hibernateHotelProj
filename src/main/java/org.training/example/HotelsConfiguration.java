be7ca1aa5247fca346d9ff96a64cf671484358f4
Artem_Kunats <Artem_Kunats@epam.com>
2018-10-07 23:23:17 +0300

    added hibernate support

--- src/main/java/org.training/example/HotelsConfiguration.java
+++ src/main/java/org.training/example/HotelsConfiguration.java
@@ -1,18 +1,24 @@
 package org.training.example;
 
+import java.util.Properties;
 import javax.sql.DataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 @PropertySource("classpath:application.properties")
+@EnableTransactionManagement
 public class HotelsConfiguration {
     @Value("${dataSource.driverClassName}")
     private String driverClassName;
@@ -26,6 +32,12 @@ public class HotelsConfiguration {
     @Value("${dataSource.password}")
     private String password;
 
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String ddl;
+
+    @Value("${hibernate.dialect}")
+    private String dialect;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -34,5 +46,30 @@ public class HotelsConfiguration {
         dataSource.setUrl(url);
         dataSource.setDriverClassName(driverClassName);
         return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean hibernateSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setPackagesToScan("my.entities.package");
+        sessionFactory.setHibernateProperties(additionalProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory);
+
+        return transactionManager;
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", ddl);
+        properties.setProperty("hibernate.dialect", dialect);
+        return properties;
     }
 }
--- src/main/java/org.training/example/dao/HotelDao.java
+++ src/main/java/org.training/example/dao/HotelDao.java
@@ -2,11 +2,16 @@ package org.training.example.dao;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.training.example.model.Hotel;
 
 @Repository
 public class HotelDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
     public List<Hotel> getHotels() {
         List<Hotel> hotels = new ArrayList<>();
         hotels.add(Hotel.builder().cityName("eee").price(222).build());

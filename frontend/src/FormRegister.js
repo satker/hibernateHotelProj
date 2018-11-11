acd95173e72ac9080ccf2b29f624b8059b2fb813
Artem_Kunats <Artem_Kunats@epam.com>
2018-11-11 22:40:14 +0300

    fixed validation

--- frontend/src/FormRegister.js
+++ frontend/src/FormRegister.js
@@ -17,9 +17,9 @@ const GOOD_PASSWORD = 'Password correct';
 const GOOD_CONFIRM_PASSWORD = '* Password correct';
 const NOT_GOOD_MAIL = 'Mail is busy';
 const NOT_GOOD_LOGIN = 'Login is busy';
-const NOT_GOOD_FIRST_NAME = 'First name not correct';
-const NOT_GOOD_LAST_NAME = 'Last name not correct';
-const NOT_GOOD_CONFIRM_PASSWORD = '* Password not correct';
+const NOT_GOOD_FIRST_NAME = 'First name cannot contain numbers';
+const NOT_GOOD_LAST_NAME = 'Last name cannot contain numbers';
+const NOT_GOOD_CONFIRM_PASSWORD = '* Passwords do not match';
 const NOT_GOOD_PASSWORD = 'Password not correct';
 const PATTERN_TO_CHECK_MAIL = '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])';
 const PATTERN_TO_CHECK_NAME = '^[A-Za-z]*$';
@@ -129,11 +129,6 @@ class FormRegister extends Component {
 
     async handleSubmit(evt) {
         evt.preventDefault();
-
-        if (this.state.password !== this.state.confirmPassword) {
-            FormRegister.error("Passwords do not match");
-            return;
-        }
 
         let body = {};
         body.login = this.state.login;
